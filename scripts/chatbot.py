2af4e57460553c394c7b622e57dc8ea23da319a0
Brandon <bwh1126@gmail.com>
2023-08-31 20:13:40 -0500

    fixed

--- scripts/chatbot.py
+++ scripts/chatbot.py
@@ -23,10 +23,55 @@ def wrap_text(text, width=MAX_WIDTH, color=None):
     wrapped_text = textwrap.fill(text, width)
     return f"[{color}]{wrapped_text}[/{color}]"
 
+
+first_input = '''
+Below is a simple SQL schema consisting of two tables: employees and departments. Your task is to write an SQL query that satisfies the given requirement.
+
+-- employees table
+CREATE TABLE employees (
+  id INT PRIMARY KEY,
+  first_name VARCHAR(50),
+  last_name VARCHAR(50),
+  salary INT,
+  department_id INT
+);
+
+-- departments table
+CREATE TABLE departments (
+  id INT PRIMARY KEY,
+  name VARCHAR(50)
+);
+
+-- Sample data in employees table
+INSERT INTO employees (id, first_name, last_name, salary, department_id) VALUES
+(1, 'John', 'Doe', 60000, 1),
+(2, 'Jane', 'Doe', 70000, 2),
+(3, 'Sally', 'Smith', 50000, 3),
+(4, 'Bill', 'Gates', 80000, 1),
+(5, 'Elon', 'Musk', 90000, 2);
+
+-- Sample data in departments table
+INSERT INTO departments (id, name) VALUES
+(1, 'Engineering'),
+(2, 'Marketing'),
+(3, 'Finance');
+
+Requirement:
+Write an SQL query to fetch the first_name, last_name, and salary of all employees who work in the "Engineering" department, sorted by salary in descending order.
+
+Please provide the SQL query that fulfills this requirement.
+'''
+
+
 while True:
     # Use rich to print the User's prompt in blue
     console.print("[blue]USER: [/blue]", end="")
-    user_input = input()
+
+    if first_input:
+        user_input = first_input
+    else:
+        user_input = input()
+    
     
     # Exit condition if user types 'exit'
     if user_input.lower() == 'exit':
@@ -35,11 +80,13 @@ while True:
     completion = openai.Completion.create(
         model="../aila-llama2-13b-hf",
         prompt=chat_prompt(user_input),
-        max_tokens=1024,
+        max_tokens=2048,
     )
     
     # Use rich to make the AI's response green and wrap it
     ai_response = wrap_text(completion['choices'][0]['text'].strip(), color="green")
     print(f"[red]AILA:[/red] {ai_response}")
+
+    first_input = None
 
 print("[yellow]Goodbye![/yellow]")
